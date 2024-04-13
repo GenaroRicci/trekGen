e8b94a43a74203558438d5b1048f975ed6965d70
Genaro Ricci <genaroricci2@gmail.com>
2024-04-13 18:39:26 -0300

    first push new trekgen

---
+++ Desktop/Coding/Flutter proyect/trek_gen/bin/trek_gen.dart
@@ -0,0 +1,5 @@
+import 'package:trek_gen/trek_gen.dart' as trek_gen;
+
+void main(List<String> arguments) {
+  print('Hello world: ${trek_gen.calculate()}!');
+}
---
+++ Desktop/Coding/Flutter proyect/trek_gen/test/trek_gen_test.dart
@@ -0,0 +1,8 @@
+import 'package:trek_gen/trek_gen.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
